4a3630d9f85c80986ffed7269a811479c31a11ef
Allen Guo <alleng@graphcore.ai>
2021-12-12 22:18:30 +0800

    day11 (#11)

---
+++ day11/p1.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+
+
+def func():
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    fp = f'{cur_dir}/input.txt'
+    # fp = f'{cur_dir}/sample.txt'
+    arr = np.loadtxt(fp, dtype=str)
+    arr = np.asarray([[np.int32(x) for x in y] for y in arr])
+    H, W = arr.shape
+
+    def get_win(h, w):
+        wins = []
+        if h > 0:
+            wins.append((h - 1, w))
+            if w > 0:
+                wins.append((h - 1, w - 1))
+            if w < W - 1:
+                wins.append((h - 1, w + 1))
+        if w > 0:
+            wins.append((h, w - 1))
+            if h < H - 1:
+                wins.append((h + 1, w - 1))
+        if w < W - 1:
+            wins.append((h, w + 1))
+            if h < H - 1:
+                wins.append((h + 1, w + 1))
+        if h < H - 1:
+            wins.append((h + 1, w))
+        return wins
+
+    flashes = 0
+    for step in range(100):
+        arr += 1
+        change = 1
+        while change > 0:
+            change -= 1
+            for h in range(H):
+                for w in range(W):
+                    if arr[h, w] > 9:
+                        flashes += 1
+                        change += 1
+                        arr[h, w] = -10
+                        for hw in get_win(h, w):
+                            arr[hw] += 1
+        arr[arr < 0] = 0
+
+    return flashes
+
+
+if __name__ == '__main__':
+    res = func()
+    print(res)
